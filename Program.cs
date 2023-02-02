6f4f1110ffc688bf84c625a91bfccd4fefbf1481
chris <chrisabelw@m3.com>
2023-02-02 16:30:11 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+Console.WriteLine("Please enter a word you would like to reverse:");
+string word = Console.ReadLine();
+
+static string Reverse(string input)
+{
+
+    char[] letters = input.ToCharArray();
+
+    Stack<char> lettersStack = new Stack<char>();
+
+    foreach (char c in letters)
+    {
+        lettersStack.Push(c);
+    }
+
+    string output = "";
+
+    foreach (char c in lettersStack)
+    {
+        output += c;
+    }    
+    return output;
+
+}
+
+
+Console.WriteLine($"Your word reversed is:");
+Console.WriteLine(Reverse(word)); 
